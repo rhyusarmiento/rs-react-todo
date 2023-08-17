be5e687840c73ee1fc9af483b1876b7b731ccea7
rhyusarmiento <rhyu.sar.15@gmail.com>
2023-08-17 11:39:44 -0600

    adding and fixing removing items

--- src/components/App.js
+++ src/components/App.js
@@ -3,19 +3,35 @@ import React, {useState} from 'react';
 function App() {
   const [allTodos, setTodos] = useState([]);
   const [newDiscription, setDiscription] = useState("");
-  const [isCompleted, setIsCompleted] = useState(false);
-  const [newDate, setDate] = useState(null);
+  const [completedTodo, setCompletedTodo] = useState([]);
   const theDate = new Date();
 
   const handleAddItem = () => {
     let newTodoItem = {
       discription:newDiscription,
-      date:`${theDate.getMonth() + 1}/${theDate.getDate()}`
+      date:`${theDate.getMonth() + 1}/${theDate.getDate()}`,
     }
 
     let updatedTodoArr = [...allTodos];
     updatedTodoArr.push(newTodoItem);
-    setTodos(updatedTodoArr)
+    setTodos(updatedTodoArr);
+  }
+
+  const handleDeleteItem = (index, test) => {
+    if (test == false) {
+      let currentList = [...allTodos];
+      currentList.splice(index, 1);
+      setTodos(currentList);
+    } else if (test == true) {
+      let unRemovedList = [...completedTodo];
+      unRemovedList.splice(index, 1);
+      setCompletedTodo(unRemovedList)
+    }
+  }
+
+  const handleCompleted = (index) => {
+    let completedList = allTodos.splice(index, 1);
+    setCompletedTodo(completedList);
   }
 
   return (
@@ -30,14 +46,26 @@ function App() {
         <div className='items-display'>    
           <div className='todo-items'>
             <h5>TODO</h5>
-            {allTodos.map((item,index) => {
+            {allTodos.map((item, index) => {
               return (
                 <div className="todo-item" key={index}>
                   <p>{item.date}</p>
                   <p>{item.discription}</p>
                   <div className="button-wrapper">
-                    <button type="button" className="completedBtn">DONE</button>
-                    <button type="button" className="removeBtn">REMOVE</button>
+                    <button 
+                      type="button" 
+                      className="completedBtn"
+                      onClick={() => handleCompleted(index)}
+                    >
+                    DONE
+                    </button>
+                    <button 
+                      type="button" 
+                      className="removeBtn"
+                      onClick={() => handleDeleteItem(index, false)}
+                      >
+                      REMOVE
+                    </button>
                   </div>
                 </div>
               )}
@@ -45,6 +73,21 @@ function App() {
           </div>
           <div className='completed-items'>
             <h5>COMPLETED</h5>
+            {completedTodo.map((item, index) => {
+              return (
+                <div className="todo-item-completed" key={index}>
+                  <p>{item.date}</p>
+                  <p>{item.discription}</p>
+                  <button 
+                    type="button" 
+                    className="removeBtn"
+                    onClick={() => handleDeleteItem(index, true)}
+                  >
+                    REMOVE
+                  </button>
+                </div>
+              )
+            })}
           </div>
         </div>
       </div>
@@ -53,4 +96,3 @@ function App() {
 }
 
 export default App;
-
